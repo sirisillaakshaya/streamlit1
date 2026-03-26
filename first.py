6304f33106d4a0c445a35bf841c017c4f80729b4
sirisillaakshaya <sirisillaakshaya145@gmail.com>
2026-03-26 10:53:27 +0530

    first commit

--- first.py
+++ first.py
@@ -1,3 +1,19 @@
 import streamlit as st
 st.title("Registration Form")
+with st.form("my_form"):
+    name1,name2=st.columns(2)
+    with name1:
+        first_name=st.text_input("First Name")
+    with name2:
+        last_name=st.text_input("Last Name")
+    email=st.text_input("EMail")
+    password1=st.text_input("password",type="password")
+    password2=st.text_input("confirm password",type="password")
+    address=st.text_area("Message")
+    submit=st.form_submit_button("Submit")
+    if(submit):
+        if(password1==password2):
+            st.success("Registration suceesfully")
+        else:
+            st.error("password does not match")
 
